f655f0a084495735faeef22fd9adfef74250103a
gillylopes <gillylopes@gmail.com>
2025-07-07 16:58:56 -0300

    fix: adicionando rota health corretamente

--- src/routers/routers.rs
+++ src/routers/routers.rs
@@ -3,10 +3,12 @@ use crate::handlers::{stake::stake_handler, unstake::unstake_handler};
 use crate::health::health_handler;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
+
+    cfg.route("/health", web::get().to(health_handler));
+
     cfg.service(
         web::scope("/staking")
             .route("/stake", web::post().to(stake_handler))
             .route("/unstake", web::post().to(unstake_handler)),
-    )
-    .route("/health", web::get().to(health_handler));
+    );
 }
